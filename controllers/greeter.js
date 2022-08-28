05a9dc67cf71dfda0fe08a3d58dd6a52fb02e4aa
De1ain <borodin.tim@gmail.com>
2022-08-28 14:44:23 +0300

    Add new route for certain person type

--- controllers/greeter.js
+++ controllers/greeter.js
@@ -1,3 +1,5 @@
+import db from '../DB/db.js';
+
 const getIndex = (req, res, next) => {
     res.json('Welcome to Birthday Greeting Message Generator!!');
 }
@@ -10,7 +12,17 @@ const getGreeting = (req, res, next) => {
     res.json(greeting);
 }
 
+const getGreetingPerson = (req, res, next) => {
+    const person = req.params.person;
+    const greeting = 'This is a dummy message for certain person - ' + person;
+
+    //TODO: Generate msg by fetching random msgs from various categories in DB and building a greeting message
+
+    res.json(greeting);
+}
+
 export {
     getIndex,
-    getGreeting
+    getGreeting,
+    getGreetingPerson
 }
--- routes/greeter.js
+++ routes/greeter.js
@@ -1,11 +1,18 @@
 import express from 'express';
 
-import { getIndex, getGreeting } from '../controllers/greeter.js';
+import {
+    getIndex,
+    getGreeting,
+    getGreetingPerson
+} from '../controllers/greeter.js';
 
 const router = express.Router();
 
 router.get('/', getIndex);
 
+router.get('/get-greeting/:person', getGreetingPerson);
+
 router.get('/get-greeting', getGreeting);
+
 
 export default router;
