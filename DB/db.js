34f52074bbc8fdbf05e31816bcb5b4e800d29647
Tim <borodin.tim@gmail.com>
2022-09-05 23:32:56 +0300

    Add simple DB with predefined greeting message elements

---
+++ DB/db.js
@@ -0,0 +1,28 @@
+const db = {
+    appeal: [
+        'dear',
+    ],
+    personType: {
+        friend: 'friend',
+        colleague: 'colleague',
+        dude: 'dude',
+        default: 'person',
+    },
+    wish: [
+        'all you wishes come true',
+        'all your dreams come true',
+        'a beautiful life',
+    ],
+    be: [
+        'loved and happy',
+        'happy every second of your life',
+        'always the coolest',
+    ],
+    and: [
+        'let there be no more problems in your life',
+        'let every single day bring you happines',
+        'that fortune will always accompany you',
+    ]
+};
+
+export { db as default }
