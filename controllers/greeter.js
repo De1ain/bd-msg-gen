790f660f98fe4ca415f817df8a0ac79864e9e8eb
Tim <borodin.tim@gmail.com>
2022-09-05 23:33:37 +0300

    Implement greeting message generator function

--- controllers/greeter.js
+++ controllers/greeter.js
@@ -14,11 +14,21 @@ const getGreeting = (req, res, next) => {
 
 const getGreetingPerson = (req, res, next) => {
     const person = req.params.person;
-    const greeting = 'This is a dummy message for certain person - ' + person;
 
-    //TODO: Generate msg by fetching random msgs from various categories in DB and building a greeting message
+    const greeting = generateGreetingMsg(person);
 
     res.status(200).json(greeting);
+}
+
+const generateGreetingMsg = (person) => {
+    const appeal = db.appeal[0];
+    const wish = db.wish[Math.floor(Math.random() * db.wish.length)]
+    const be = db.be[Math.floor(Math.random() * db.be.length)]
+    const and = db.and[Math.floor(Math.random() * db.and.length)]
+
+    const greeting = `${appeal} ${person}, I wish you ${wish}, be ${be} and ${and}`;
+
+    return greeting;
 }
 
 export {
