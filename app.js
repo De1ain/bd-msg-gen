edc21b8500139ea1b30b5496a65ee9393477099c
Tim <borodin.tim@gmail.com>
2022-09-05 23:32:27 +0300

    Allow CORS, allow get requests from the server

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = 3003;
 
 const app = express();
 
+app.use((_, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    next();
+});
+
 app.use(router);
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
